4e293a2a0f52502b5bc57080ccea9bdd52eab51a
freybupt <freybupt@gmail.com>
2013-04-21 19:09:00 -0700

    Initial Commit

---
+++ Tetris360/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Tetris360
+//
+//  Created by Liang Shi on 2013-04-21.
+//  Copyright (c) 2013 Gree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Tetris360/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Tetris360
+//
+//  Created by Liang Shi on 2013-04-21.
+//  Copyright (c) 2013 Gree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
