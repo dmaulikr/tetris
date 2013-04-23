f84b750b0e1d9e5711d9b55dc2b9d3d247e9b1eb
Liang Shi <liang.shi@gree.co.jp>
2013-04-22 17:01:23 -0700

    init commit

---
+++ Tetris360/PieceView.h
@@ -0,0 +1,13 @@
+//
+//  PieceView.h
+//  Tetris360
+//
+//  Created by Liang Shi on 4/22/13.
+//  Copyright (c) 2013 Gree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PieceView : UIView
+
+@end
---
+++ Tetris360/PieceView.m
@@ -0,0 +1,31 @@
+//
+//  PieceView.m
+//  Tetris360
+//
+//  Created by Liang Shi on 4/22/13.
+//  Copyright (c) 2013 Gree. All rights reserved.
+//
+
+#import "PieceView.h"
+
+@implementation PieceView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
