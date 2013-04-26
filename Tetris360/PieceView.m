827eb4d609b75b21552ada9eda7439577ddebf4f
Liang Shi <liang.shi@gree.co.jp>
2013-04-26 14:36:11 -0700

    add tutorial

--- Tetris360/PieceView.m
+++ Tetris360/PieceView.m
@@ -168,135 +168,74 @@
 
 #pragma mark - touch events
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
-
-    if (self.pieceRotated == PieceRotateThreeTimes) {
-        self.pieceRotated = PieceOriginal;
-    }
-    else{
-        self.pieceRotated++;
-    }
-
-    //change frame of current view for rotation - doesn't work
-//    CGRect frame;
-//    switch (self.pieceType) {
-//        case PieceTypeI:
-//            if (self.pieceRotated%2 == 0) {
-//                frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, kGridSize * 4, kGridSize * 1);
-//            }
-//            else{
-//                frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, kGridSize * 1, kGridSize * 4);
-//            }
-//            break;
-//        case PieceTypeJ:
-//        case PieceTypeL:
-//        case PieceTypeS:
-//        case PieceTypeT:
-//        case PieceTypeZ:
-//            if (self.pieceRotated%2 == 0) {
-//                frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, kGridSize * 3, kGridSize * 2);
-//            }
-//            else{
-//                frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, kGridSize * 2, kGridSize * 3);
-//            }
-//            break;
-//        default:
-//            break;
-//    }
-//    self.frame = frame;
-
-
-    //rotate the piece 90 degrees clockwise
-    if (self.pieceType == PieceTypeI) {
-        if (self.pieceRotated%2 == 0) {
-            CGPoint newPoint = CGPointMake(0, 1);
-            self.blocksCenter[1] = [NSValue valueWithCGPoint:newPoint];
-            newPoint = CGPointMake(2, 1);
-            self.blocksCenter[2] = [NSValue valueWithCGPoint:newPoint];
-            newPoint = CGPointMake(3, 1);
-            self.blocksCenter[3] = [NSValue valueWithCGPoint:newPoint];
+    if (self.pieceRotated != PieceRotateStoped) {
+        if (self.pieceRotated == PieceRotateThreeTimes) {
+            self.pieceRotated = PieceOriginal;
         }
         else{
-            CGPoint newPoint = CGPointMake(1, 0);
-            self.blocksCenter[1] = [NSValue valueWithCGPoint:newPoint];
-            newPoint = CGPointMake(1, 2);
-            self.blocksCenter[2] = [NSValue valueWithCGPoint:newPoint];
-            newPoint = CGPointMake(1, 3);
-            self.blocksCenter[3] = [NSValue valueWithCGPoint:newPoint];
+            self.pieceRotated++;
         }
-    }
-    else if (self.pieceType != PieceTypeO) {
-        for (int i = 1; i < kNUMBER_OF_BLOCKS; i++) {
-            int x = [self.blocksCenter[i] CGPointValue].x;
-            int y = [self.blocksCenter[i] CGPointValue].y;
 
-            //up down left right
-            if (x == 1 && y == 0) {
-                x = 2; y = 1;
+
+        //rotate the piece 90 degrees clockwise
+        if (self.pieceType == PieceTypeI) {
+            if (self.pieceRotated%2 == 0) {
+                CGPoint newPoint = CGPointMake(0, 1);
+                self.blocksCenter[1] = [NSValue valueWithCGPoint:newPoint];
+                newPoint = CGPointMake(2, 1);
+                self.blocksCenter[2] = [NSValue valueWithCGPoint:newPoint];
+                newPoint = CGPointMake(3, 1);
+                self.blocksCenter[3] = [NSValue valueWithCGPoint:newPoint];
             }
-            else if (x == 2 && y == 1){
-                x = 1;  y = 2;
+            else{
+                CGPoint newPoint = CGPointMake(1, 0);
+                self.blocksCenter[1] = [NSValue valueWithCGPoint:newPoint];
+                newPoint = CGPointMake(1, 2);
+                self.blocksCenter[2] = [NSValue valueWithCGPoint:newPoint];
+                newPoint = CGPointMake(1, 3);
+                self.blocksCenter[3] = [NSValue valueWithCGPoint:newPoint];
             }
-            else if(x == 1 && y == 2){
-                x = 0; y = 1;
-            }
-            else if(x == 0 && y == 1){
-                x = 1; y = 0;
-            }
-            //for corners
-            if (x == 0 && y == 0) {
-                x = 2; y = 0;
-            }
-            else if (x == 2 && y == 0){
-                x = 2;  y = 2;
-            }
-            else if(x == 2 && y == 2){
-                x = 0; y = 2;
-            }
-            else if(x == 0 && y == 2){
-                x = 0; y = 0;
-            }
-            
-            CGPoint newPoint = CGPointMake(x, y);
-            self.blocksCenter[i] = [NSValue valueWithCGPoint:newPoint];
         }
+        else if (self.pieceType != PieceTypeO) {
+            for (int i = 1; i < kNUMBER_OF_BLOCKS; i++) {
+                int x = [self.blocksCenter[i] CGPointValue].x;
+                int y = [self.blocksCenter[i] CGPointValue].y;
+
+                //up down left right
+                if (x == 1 && y == 0) {
+                    x = 2; y = 1;
+                }
+                else if (x == 2 && y == 1){
+                    x = 1;  y = 2;
+                }
+                else if(x == 1 && y == 2){
+                    x = 0; y = 1;
+                }
+                else if(x == 0 && y == 1){
+                    x = 1; y = 0;
+                }
+                //for corners
+                if (x == 0 && y == 0) {
+                    x = 2; y = 0;
+                }
+                else if (x == 2 && y == 0){
+                    x = 2;  y = 2;
+                }
+                else if(x == 2 && y == 2){
+                    x = 0; y = 2;
+                }
+                else if(x == 0 && y == 2){
+                    x = 0; y = 0;
+                }
+                
+                CGPoint newPoint = CGPointMake(x, y);
+                self.blocksCenter[i] = [NSValue valueWithCGPoint:newPoint];
+            }
+        }
+        
+        
+        [self setNeedsDisplay];
     }
-
-
-    [self setNeedsDisplay];
-
-    
-//    self.layer.anchorPoint = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
-//    NSLog(@"AnchorPoint: x %f   y %f", self.layer.anchorPoint.x, self.layer.anchorPoint.y);
-//    if (self.pieceType != PieceTypeO) {
-//        // Repositions and resizes the view.
-//        [UIView beginAnimations:@"rotate" context:nil];
-//        [UIView setAnimationDuration:0.1];
-//        
-//        switch (self.pieceRotated) {
-//            case PieceOriginal:
-//                self.transform = CGAffineTransformMakeRotation(DegreesToRadians(90));
-//                [self setFrame:CGRectMake(self.frame.origin.x + kGridSize / 2.0, self.frame.origin.y + kGridSize / 2.0, self.frame.size.width, self.frame.size.height)];
-//                self.pieceRotated = PieceRotateOnce;
-//                break;
-//            case PieceRotateOnce:
-//                self.transform = CGAffineTransformMakeRotation(DegreesToRadians(180));
-//                self.pieceRotated = PieceRotateTwice;
-//                break;
-//            case PieceRotateTwice:
-//                self.transform = CGAffineTransformMakeRotation(DegreesToRadians(270));
-//                [self setFrame:CGRectMake(self.frame.origin.x - kGridSize / 2.0, self.frame.origin.y - kGridSize / 2.0, self.frame.size.width, self.frame.size.height)];
-//                self.pieceRotated = PieceRotateThreeTimes;
-//                break;
-//            case PieceRotateThreeTimes:
-//                self.transform = CGAffineTransformMakeRotation(DegreesToRadians(0));
-//                self.pieceRotated = PieceOriginal;
-//                break;
-//            default:
-//                break;
-//        }
-//
-//        [UIView commitAnimations];
-//    }
 }
 
 @end
